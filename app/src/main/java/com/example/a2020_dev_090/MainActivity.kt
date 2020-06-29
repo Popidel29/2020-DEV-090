18e01022b46d6d9565f9ad2bb47d4a56e12397a5
Popidel29 <idelsilgon29@hotmail.com>
2020-06-29 18:21:27 +0100

    Application User Interface created: Gaming board customization Buttons created along with the specified position, size and colours.

--- app/src/main/java/com/example/a2020_dev_090/MainActivity.kt
+++ app/src/main/java/com/example/a2020_dev_090/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.a2020_dev_090
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnClick(view: View) {
+
     }
 }
