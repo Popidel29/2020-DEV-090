a68ca5db56e02dcb142ce59dfb8af43dbb4f2e28
Popidel29 <idelsilgon29@hotmail.com>
2020-06-29 19:13:37 +0100

    Create Unit Test Model and View Model skeleton: Classes, properties and functions

--- app/src/main/java/com/example/a2020_dev_090/model/GameRepository.kt
+++ app/src/main/java/com/example/a2020_dev_090/model/GameRepository.kt
@@ -1,23 +1,83 @@
 package com.example.a2020_dev_090.model
 
-class GameRepository(playerOneName : String, playerTwoName: String){
+import com.example.a2020_dev_090.R
 
-    fun playGame(x : Int,  y: Int): Player? {
-        return null
+class GameRepository(playerOneName: String, playerTwoName: String) {
+
+    private val playerOne = Player(playerOneName, "X", R.color.green)
+    private var playerTwo = Player(playerTwoName, "O", R.color.red)
+    private var activePlayer = playerOne
+    private var moveCount = 0
+
+    private var board = Array(3) { Array(3) { Player("", "", R.color.white) } }
+
+    fun playGame( x : Int,  y: Int) : Player?
+    {
+        if(board[x][y].name !=  "")
+        {
+            return null
+        }
+
+        moveCount++
+        board[x][y] =  activePlayer
+        return  activePlayer
     }
 
-    fun checkWinner():Player? {
-    return null
-    }
-    fun switchPlayer() {
-    }
-
-    fun isDraw(): Boolean{
-      return false
-    }
-    fun resetGame() {
-
+    fun switchPlayer(){
+        activePlayer = if(activePlayer == playerOne){
+            playerTwo
+        }else{
+            playerOne
+        }
     }
 
+    fun isDraw() : Boolean
+    {
+        return moveCount == 9
+    }
 
+    fun checkWinner() : Player? {
+        var winner : Player ?= null
+        //check row
+        for(i in 0..2){
+            if(board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+                winner = board[i][0]
+                break
+            }
+        }
+        //check col
+        for(i in 0..2){
+            if(board[0][i] == board[1][i] && board[1][i] == board[2][i]){
+                winner = board[1][i]
+                break
+            }
+        }
+        //check diagonal
+        for(i in 1..2){
+            if(board[0][0] != board[i][i])
+            {
+                break
+            }
+            if (i == 2) {
+                winner = board[0][0]
+            }
+        }
+        //check anti- diagonal
+        for(i in 1..2){
+            if (board[i][2 -i] != board[0][2]) {
+                break
+            }
+            if (i == 2) {
+                winner = board[0][2]
+            }
+        }
+        switchPlayer()
+        return  winner
+    }
+    fun resetGame()
+    {
+        activePlayer = playerOne
+        board = Array(3){Array(3){Player("","", R.color.white)} }
+        moveCount = 0
+    }
 }
