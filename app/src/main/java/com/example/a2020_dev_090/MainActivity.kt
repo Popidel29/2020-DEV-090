88c445e034558fe2ecd80148a3e22276f38b7784
Popidel29 <idelsilgon29@hotmail.com>
2020-06-29 12:06:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a2020_dev_090/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a2020_dev_090
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
