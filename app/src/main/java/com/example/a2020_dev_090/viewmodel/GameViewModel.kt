eabc4a9a23d0db28743673ae4ecb25cb039f6e73
Popidel29 <idelsilgon29@hotmail.com>
2020-06-29 19:28:59 +0100

    Game View Model Implementation
    Create Game ViewModel Factory

--- app/src/main/java/com/example/a2020_dev_090/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/example/a2020_dev_090/viewmodel/GameViewModel.kt
@@ -2,10 +2,11 @@ package com.example.a2020_dev_090.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.a2020_dev_090.model.GameRepository
 import com.example.a2020_dev_090.model.Player
 
-class GameViewModel(private val gameRepository: GameRepository) {
+class GameViewModel(private val gameRepository: GameRepository): ViewModel() {
 
     val activePlayer : LiveData<Player>
         get() = mActivePlayer
@@ -15,18 +16,35 @@ class GameViewModel(private val gameRepository: GameRepository) {
         get() = mGameResult
     private var mGameResult = MutableLiveData<String>()
 
-    fun playGame(x: Int,  y: Int){
+    fun playGame( x: Int,  y: Int)
+    {
+        mActivePlayer.postValue(gameRepository.playGame(x,y))
 
     }
 
-    fun checkWinner(){
+    fun checkWinner()
+    {
+        var winnerPlayer : Player? = gameRepository.checkWinner()
 
+        if(winnerPlayer == null && gameRepository.isDraw())
+        {
+            mGameResult.postValue("Draw")
+            return
+        }
+
+        if(winnerPlayer!= null)
+        {
+            mGameResult.postValue(winnerPlayer.name)
+            return
+        }
     }
 
-    fun resetGame(){
-
+    fun resetGame()
+    {
+        gameRepository.resetGame()
     }
+
     fun switchPlayer() {
+        gameRepository.switchPlayer()
     }
-
 }
